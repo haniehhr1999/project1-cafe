3c2a5ba921c0853b80a997f30335d5bcd7bb802c
hanieh <haniehrahim78@gmail.com>
2025-02-25 15:07:45 +0330

    navbar

--- src/components/modules/navbar/Navbar.jsx
+++ src/components/modules/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='bg-orange-200 rounded-md'>
+      <ul className='flex'>
+        <li className='mx-3'>
+          <Link href='/'>Home</Link>
+        </li>
+        <li className='mx-3'>
+          <Link href='/'>About</Link>
+        </li>
+        <li className='mx-3'>
+          <Link href='/'>Service</Link>
+        </li>
+        <li className='mx-3'>
+          <Link href='/'>Menu</Link>
+        </li>
+        <li className='mx-3'>
+          <Link href='/'>Pages</Link>
+        </li>
+        <li className='mx-3'>
+          <Link href='/'>Contacts</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
