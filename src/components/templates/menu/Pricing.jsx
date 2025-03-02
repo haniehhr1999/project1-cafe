f8fe4cf407e75bf7ac10c0cebfab8cfd1a191914
hanieh <haniehrahim78@gmail.com>
2025-03-03 00:31:50 +0330

    menu page ISR

--- src/components/templates/menu/Pricing.jsx
+++ src/components/templates/menu/Pricing.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Pricing = () => {
+const Pricing = ({ data }) => {
+  console.log("data", data);
   return (
-    <div>Pricing</div>
-  )
-}
+    <div>
+      <h1>menu</h1>
+      <h1>hot bar</h1>
+      <div className="grid grid-cols-3 gap-8 px-40">
+        {data
+          ?.filter((item) => item.type === "hot")
+          .map((item) => (
+            <div className="shadow-md rounded-md">
+              <img className="w-full rounded-md" src={item.img} alt="" />
+              <div className="p-5">
+                <h2>
+                  {item.title} - {item.price}
+                </h2>
+                <p>{item.desc}</p>
+              </div>
+            </div>
+          ))}
+        
+      </div>
 
-export default Pricing
+
+      <h1>cold bar</h1>
+      <div className="grid grid-cols-3 gap-8 px-40">
+        {data
+          ?.filter((item) => item.type === "cold")
+          .map((item) => (
+            <div className="shadow-md rounded-md">
+              <img className="w-full rounded-md" src={item.img} alt="" />
+              <div className="p-5">
+                <h2>
+                  {item.title} - {item.price}
+                </h2>
+                <p>{item.desc}</p>
+              </div>
+            </div>
+          ))}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,9 +1,28 @@
+import PageHeader from '@/components/modules/pageHeader/PageHeader'
+import Pricing from '@/components/templates/menu/Pricing'
 import React from 'react'
 
-const Menu = () => {
+const Menu = ({menu}) => {
+  console.log(menu , 'menu')
   return (
-    <div>Menu</div>
+    <div>
+      <PageHeader route='menu' />
+      <Pricing data = {menu} />
+    </div>
   )
+}
+
+export async function getStaticProps(params) {
+
+  const res = await fetch('http://localhost:9000/menu')
+  const menu = await res.json()
+  console.log(menu)
+  return{
+    props:{
+      menu
+    } , 
+    revalidate : 60 * 60*12
+  }
 }
 
 export default Menu
